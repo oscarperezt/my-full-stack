ff7d98761e0ec351037a76af463f11e0534519a7
Oscar Perez <oscar@coltrack.com>
2024-09-28 16:39:48 -0400

    Added reports route to the api

--- backend/app/api/main.py
+++ backend/app/api/main.py
@@ -1,9 +1,11 @@
+import re
 from fastapi import APIRouter
 
-from app.api.routes import items, login, users, utils
+from app.api.routes import items, login, users, utils, reports
 
 api_router = APIRouter()
 api_router.include_router(login.router, tags=["login"])
 api_router.include_router(users.router, prefix="/users", tags=["users"])
 api_router.include_router(utils.router, prefix="/utils", tags=["utils"])
 api_router.include_router(items.router, prefix="/items", tags=["items"])
+api_router.include_router(reports.router, prefix="/reports", tags=["reports"])
